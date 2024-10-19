325baa948b76819158e1189e0e06076b40637a71
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-10-19 11:48:51 +0800

    committing before restart

--- Scripts/TransitionTo1v1.gd
+++ Scripts/TransitionTo1v1.gd
@@ -2,12 +2,13 @@ extends GridContainer
 
 func _ready():
 	for child in get_children():
-		child.gui_input.connect(accessClicked.bind(child.name))
+		var chosenChara = child.get_child(0)
+		chosenChara.gui_input.connect(accessClicked.bind(chosenChara))
 
-func accessClicked(event, name):
+func accessClicked(event, node):
 	if event is InputEventMouseButton \
 	and event.button_index == MOUSE_BUTTON_LEFT \
 	and event.pressed:
-		print(name)
+		print(node.name)
 
 
